ec2190a90625e78b23a1fe75f3f2a870f01b04d3
xuxuyao_mixifun <977905894040160-xuxuyao_mixifun@noreply.coze.cn>
2026-03-31 14:20:32 +0800

    feat: 重构 buildMaterialLhCapacityMap 从基础表查询日硫化产能

---
+++ src/main/java/com/zlt/aps/mp/api/mapper/MdmMonthPlanProductLhCapacityMapper.java
@@ -0,0 +1,15 @@
+package com.zlt.aps.mp.api.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zlt.aps.mp.api.domain.entity.MdmMonthPlanProductLhCapacity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 工厂月度排产-SKU日硫化产能Mapper接口
+ *
+ * @author APS Team
+ */
+@Mapper
+public interface MdmMonthPlanProductLhCapacityMapper extends BaseMapper<MdmMonthPlanProductLhCapacity> {
+
+}
