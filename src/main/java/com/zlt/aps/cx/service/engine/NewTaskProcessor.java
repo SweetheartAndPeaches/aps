bf5e76e92b2b0b1af3b60f5b59556a092bcd1b61
xuxuyao_mixifun <977905894040160-xuxuyao_mixifun@noreply.coze.cn>
2026-04-03 11:45:00 +0800

    refactor: 将 MpCxCapacityConfiguration 移动到正确的包路径
    主要修改：
    1. 将 MpCxCapacityConfiguration 从 com.zlt.aps.cx.entity 移动到 com.zlt.aps.mp.api.domain.entity
    2. 更新所有引用该类的文件的导入语句：
       - MpCxCapacityConfigurationMapper.java
       - BalancingService.java
       - ProductionCalculator.java
       - TrialTaskProcessor.java
       - ContinueTaskProcessor.java
       - NewTaskProcessor.java
       - ScheduleServiceImpl.java
       - ScheduleContextVo.java
    3. 删除旧文件

--- src/main/java/com/zlt/aps/cx/service/engine/NewTaskProcessor.java
+++ src/main/java/com/zlt/aps/cx/service/engine/NewTaskProcessor.java
@@ -1,7 +1,7 @@
 package com.zlt.aps.cx.service.engine;
 
 import com.zlt.aps.cx.vo.ScheduleContextVo;
-import com.zlt.aps.cx.entity.MpCxCapacityConfiguration;
+import com.zlt.aps.mp.api.domain.entity.MpCxCapacityConfiguration;
 import com.zlt.aps.cx.entity.config.CxShiftConfig;
 import com.zlt.aps.cx.entity.config.CxStructurePriority;
 import com.zlt.aps.mp.api.domain.entity.MdmMoldingMachine;
